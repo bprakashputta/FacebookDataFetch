2c68d5e16f41612983e7bb1a99f49c597ba73be9
Putta Bhanu Prakash <bhanuprakash.putta1999@gmail.com>
2021-04-21 17:45:23 +0530

    Update GetPermanentFacebookAccessToken.java

--- src/main/java/facebook/lead/push/by/pageid/global/GetPermanentFacebookAccessToken.java
+++ src/main/java/facebook/lead/push/by/pageid/global/GetPermanentFacebookAccessToken.java
@@ -3,11 +3,13 @@ package facebook.lead.push.by.pageid.global;
 import org.mortbay.util.ajax.JSON;
 
 public class GetPermanentFacebookAccessToken {
-//    https://graph.facebook.com/v9.0/oauth/access_token?grant_type=fb_exchange_token&client_id=179407979328264&client_secret=c653ee328fcd9b2449fdfc8d7c53e996&fb_exchange_token=EAACjK6zgswgBACTVyZCzHkSdEvm9ptsXR7BKvPKQ8LNB3787RKUa6oPZBmG2I3xtL4jbUoUO45ZAru6B6CYKZAbF9AtD4CmpCkiwxJ5yhXusY5O8NfjVt2vO6xiYeZA3RTZAySpE7WhuQe4QZCFau4ELBki0bKPnZCAVNVibpTcQmRUKMiUIibo0aOR0HQRmwX9YI9ZAcVf4VqLFaXxjWH4fJ5v5s0QjtQliZCiHkLHBjGI2x72yCd8rJz
+//    https://graph.facebook.com/v9.0/oauth/access_token?grant_type=fb_exchange_token&client_id={}&client_secret={}&fb_exchange_token={}
     public static String API_URL = "https://graph.facebook.com/v9.0";
-    public static String temporaryUserAccessToken = "EAACjK6zgswgBAFQc3O8b6mLyZACpUka4cRS42xOGPoBGj7puwnkAFGiLQJfZBAUbMGvvdG65e2oZCPDji4NAOoLxKtb4ntsKPhwJK0TFZCHiyB0caXiiROCkdfxCzUe4smdFsZCBX1ZCZCqu5hn5jGP9VADoPWOVtZBO8LQb9nxZAvrzW6PcO3hBGtoNuRP8dZBVErNVNgVMyzA7SzpMbuD3zFa8mqQe1Sxim6VdUdLCEfoWF7Yq1p4UZB0HOMWobAQqxcZD";
-    public static String client_id = "179407979328264";
-    public static String client_secret = "c653ee328fcd9b2449fdfc8d7c53e996";
+    //Get Temporary Access token to your facebook account from Facebook for Developers
+    public static String temporaryUserAccessToken = "";
+    // Get the client_id and client_secret from your project
+    public static String client_id = "";
+    public static String client_secret = "";
     public static String oAuth = "/oauth/access_token?grant_type=fb_exchange_token";
 
     public static String getPermanentUserAccessToken(String temporaryUserAccessToken){
